05b87a5428c0c09eda67bcf77356dda2faba276c
luzmila cuello04 <luzmicuello22@gmail.com>
2026-09-09 18:50:54 -0300

    Add files via upload
    Hola juani y pablo , como  estan ?. 
    la verdad no puede terminar el trabajo ya que renege a mas no poder y al final no lo pude solucionar .

---
+++ Socio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoClubExamen.Modelos
+{
+    public class Socio
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public required string Dni { get; set; }
+        public string? Telefono { get; set; }
+        public ICollection<Alquiler> Alquileres { get; set; } = new List<Alquiler>();
+    }
+}
---
+++ VideoclubContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VideoClub.AccesoDatos.Modelos;
+
+namespace VideoClubExamen.Modelos
+{
+    public class VideoclubContext : DbContext
+    {
+        public DbSet<Socio> Socios { get; set; }
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Alquiler> Alquileres { get; set; }
+        public DbSet<AlquilerPelicula> AlquilerPeliculas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=videoclub.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AlquilerPelicula>()
+                .HasKey(ap => new { ap.AlquilerId, ap.PeliculaId });
+        }
+    }
+}
